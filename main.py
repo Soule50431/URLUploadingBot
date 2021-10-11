b0d4cdd8233b34351ac1909f71ef111672d7177a
Soule50431 <s.nove.trece.50431@gmail.com>
2021-10-12 00:43:03 +0900

    Create files

---
+++ main.py
@@ -0,0 +1,65 @@
+import random
+
+from bs4 import BeautifulSoup
+import discord
+
+from notion_api import *
+
+# 自分のBotのアクセストークンに置き換えてください
+DISCORD_TOKEN = os.getenv("DiscordToken")
+NOTION_TOKEN = os.getenv("NotionToken")
+
+channel_ids = list(map(int, os.getenv("ChannelIDs").split(" ")))
+database_ids = os.getenv("DatabaseIDs").split(" ")
+
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
+
+
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('logged in')
+
+
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # ignore when sender is bot
+    if message.author.bot:
+        return
+
+    if message.content == "/hootle":
+        rand = random.random()
+        if rand < 0.3:
+            await message.channel.send("フルー!")
+        elif 0.3 <= rand < 0.6:
+            await message.channel.send("フルッフ!")
+        else:
+            await message.channel.send("フルル!")
+
+    if "https://" in message.content:
+
+        for channel_id, database_id in zip(channel_ids, database_ids):
+            if message.channel.id == channel_id:
+                # get url and its title
+                url = message.content
+
+                stop_words = ["youtube", "twitter", "nicovideo", "github"]
+                if not any([stop_word in url for stop_word in stop_words]):
+                    html = requests.get(url).text
+
+                    soup = BeautifulSoup(html, "html.parser")
+                    title = soup.title.string
+                else:
+                    title = url
+
+                if url not in get_page_urls(database_id):
+                    post_new_pages(database_id, title, url)
+                    print(f"create new page of {title}")
+                else:
+                    print(f"{title} already exists")
+
+# Botの起動とDiscordサーバーへの接続
+client.run(DISCORD_TOKEN)
---
+++ notion_api.py
@@ -0,0 +1,53 @@
+import requests
+import os
+import json
+
+NOTION_TOKEN = os.getenv("NotionToken")
+headers = {"Authorization": f"Bearer {NOTION_TOKEN}",
+           "Content-Type": "application/json",
+           "Notion-Version": "2021-05-13",
+           }
+
+
+def get_request_url(end_point):
+    return f'https://api.notion.com/v1/{end_point}'
+
+
+def get_page_urls(database_id):
+    body = {
+        "parent": {
+            "database_id": database_id
+            }
+    }
+    results = requests.request("POST",
+                               url=get_request_url(f'databases/{database_id}/query'),
+                               headers=headers,
+                               data=json.dumps(body)).json()
+
+    page_urls = list()
+    for result in results["results"]:
+        page_urls.append(result["properties"]["URL"]["url"])
+
+    return page_urls
+
+
+def post_new_pages(database_id, title, url):
+    body = {
+        "parent": {
+            "database_id": database_id
+        },
+        "properties": {
+            "Name": {"title": [
+                {"text": {"content": title}}
+            ]},
+            "URL": {
+                "url": url
+            }
+        }
+    }
+    result = requests.request("POST", url=get_request_url(f'pages'), headers=headers,  data=json.dumps(body)).json()
+
+    if result["object"] == "error":
+        return False
+    else:
+        return True
